2c7ab74e4b1351ac8032ada4b15c786e313e92fe
rodrigo <rodrigopalominocastro0@gmail.com>
2024-09-25 16:01:08 -0500

    arreglando errores

--- src/models/factuta.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "sequelize";
-import { Factura as FacturaInterface } from "../interface/factura";
-import sequelize from "../db/connection";
-
-export interface FacturaModel
-  extends Model<FacturaInterface>,
-    FacturaInterface {}
-
-export const Factura = sequelize.define;
