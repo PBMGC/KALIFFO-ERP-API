8e21ea3cd405e5b9e8fff478180f85a80f652480
rodrigo <rodrigopalominocastro0@gmail.com>
2024-09-25 13:46:22 -0500

    agregando procedimientos almacenados

--- src/util/initProcedure.ts
+++ src/util/initProcedure.ts
@@ -1,19 +1,29 @@
 import sequelize from "../db/connection";
 
-const createProcedure = async () => {
-  await sequelize.query(`
-    CREATE PROCEDURE SumarUnoMasUno()
-    BEGIN
-      SELECT 5 + 1 AS Resultado;
-    END;
-  `);
+const crearProcedimiento = async () => {
+  try {
+    await sequelize.query(`
+      CREATE PROCEDURE SumarUnoMasUno()
+      BEGIN
+        SELECT 5 + 1 AS Resultado;
+      END;
+    `);
+  } catch (error) {
+    console.log("error al crear el procedimiento");
+    console.error(error);
+  }
 };
 
-const borrarProcedure = async () => {
-  await sequelize.query(`DROP PROCEDURE IF EXISTS SumarUnoMasUno;`);
+const eliminarProcedimiento = async () => {
+  try {
+    await sequelize.query(`DROP PROCEDURE IF EXISTS SumarUnoMasUno;`);
+  } catch (error) {
+    console.log("error al eliminar procedimientos");
+    console.error(error);
+  }
 };
 
 export const initProcedure = async () => {
-  await borrarProcedure();
-  await createProcedure();
+  await eliminarProcedimiento();
+  await crearProcedimiento();
 };
