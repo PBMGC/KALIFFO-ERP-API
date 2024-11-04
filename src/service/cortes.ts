f13598723ad9db5e8d46a86f65dabb819c0b141b
rodrigo <rodrigopalominocastro0@gmail.com>
2024-11-04 12:52:04 -0500

    cree bien las rutas

--- src/service/cortes.ts
+++ src/service/cortes.ts
@@ -86,7 +86,6 @@ export const _getCortesPorLote = async (lote_id: number) => {
     const queryText = `SELECT cortes.corte_id, usuario.nombre,producto.nombre,cortes.cantidad_enviada,cortes.cantidad_recibida,cortes.talla,cortes.metraje_asignado,cortes.tipo_tela FROM cortes inner JOIN
 usuario on cortes.taller_id = usuario.usuario_id INNER JOIN producto on producto.producto_id = cortes.producto_id where cortes.lote_id=?`;
     const result = await query(queryText, [lote_id]);
-
     if (result.data && result.data.length === 0) {
       return {
         message: "Cortes no encontrados.",
